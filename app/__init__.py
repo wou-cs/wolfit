bfc97297fc1bfe1e49f4fa4c86a6d0b26875bc53
Chris Brooks <chris@chrisbrooks.org>
2018-06-17 13:52:52 -0400

    100% test coverage

--- app/__init__.py
+++ app/__init__.py
@@ -9,11 +9,6 @@ import config
 app = Flask(__name__)
 app.config.from_object(config.Config)
 app.config.from_envvar('WOLFIT_SETTINGS')
-
-SECRET_KEY = app.config['SECRET_KEY']
-if not SECRET_KEY:
-    raise ValueError("No secret key set for Flask application")
-
 app.config['SQLALCHEMY_DATABASE_URI'] = config.Config.DATABASE_URI(app)
 
 db = SQLAlchemy(app)
