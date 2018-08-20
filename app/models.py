5897ef89549e0757c4e3b25f10befbdb4362b03a
Chris Brooks <chris@chrisbrooks.org>
2018-08-20 09:09:04 -0400

    Using pretty date formatting and basing everything off UTC

--- app/models.py
+++ app/models.py
@@ -1,8 +1,52 @@
 from datetime import datetime
+from dateutil import tz
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 import markdown
 from app import db, login
+
+
+def pretty_date(time=False):
+    """
+    Get a datetime object or a int() Epoch timestamp and return a
+    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc
+    """
+    now = datetime.utcnow()
+    if type(time) is int:
+        diff = now - datetime.fromtimestamp(time)
+    elif isinstance(time, datetime):
+        diff = now - time
+    elif not time:
+        diff = now - now
+    second_diff = diff.seconds
+    day_diff = diff.days
+
+    if day_diff < 0:
+        return 'just about now'
+
+    if day_diff == 0:
+        if second_diff < 10:
+            return "just now"
+        if second_diff < 60:
+            return str(second_diff) + " seconds ago"
+        if second_diff < 120:
+            return "a minute ago"
+        if second_diff < 3600:
+            return str(second_diff // 60) + " minutes ago"
+        if second_diff < 7200:
+            return "an hour ago"
+        if second_diff < 86400:
+            return str(second_diff // 3600) + " hours ago"
+    if day_diff == 1:
+        return "Yesterday"
+    if day_diff < 7:
+        return str(day_diff) + " days ago"
+    if day_diff < 31:
+        return str(day_diff // 7) + " weeks ago"
+    if day_diff < 365:
+        return str(day_diff // 30) + " months ago"
+    return str(day_diff // 365) + " years ago"
 
 
 class User(UserMixin, db.Model):
@@ -43,6 +87,9 @@ class Post(db.Model):
 
     def body_as_html(self):
         return markdown.markdown(self.body)
+
+    def pretty_timestamp(self):
+        return pretty_date(self.timestamp)
 
 
 @login.user_loader
