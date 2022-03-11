0432f4aea8b24ef2a53bcfda8216e44e1266d17c
MentalAbuseToHoomans <63027333+KittyWitty2019@users.noreply.github.com>
2022-03-11 15:09:22 -0800

    Testing helpers
    Further added testing for days, weeks, months, and years

--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -1,12 +1,13 @@
 from app.helpers import pretty_date
 from datetime import datetime, timedelta
 
+#++ Testing for timestamps in the past day
 def test_now():
     assert (pretty_date(datetime.utcnow())) == "just now"
 
 
 def test_seconds():
-    assert (pretty_date(datetime.utcnow() - timedelta(seconds=59))) == " seconds ago"
+    assert (pretty_date(datetime.utcnow() - timedelta(seconds=59))) == "59 seconds ago"
 
 
 def test_minute():
@@ -14,7 +15,7 @@ def test_minute():
 
 
 def test_minutes():
-    assert (pretty_date(datetime.utcnow() - timedelta(seconds=3599))) == " minutes ago"
+    assert (pretty_date(datetime.utcnow() - timedelta(seconds=3599))) == "59 minutes ago"
 
 
 def test_hour():
@@ -22,8 +23,36 @@ def test_hour():
 
 
 def test_hours():
-    assert (pretty_date(datetime.utcnow() - timedelta(seconds=86399))) == " hours ago"
+    assert (pretty_date(datetime.utcnow() - timedelta(seconds=86399))) == "23 hours ago"
+
+#--
+def test_day_now():
+    assert (pretty_date(datetime.utcnow() - timedelta(days=0))) == "just about now"
 
 
-def test_yesterday():
+def test_day_yesterday():
     assert (pretty_date(datetime.utcnow() - timedelta(days=1))) == "Yesterday"
+
+
+def test_day_days():
+    assert (pretty_date(datetime.utcnow() - timedelta(days=4))) == "4 days ago"
+
+
+def test_day_weeks():
+    assert (pretty_date(datetime.utcnow() - timedelta(days=15))) == "2 weeks ago"
+
+
+def test_day_months():
+    assert (pretty_date(datetime.utcnow() - timedelta(days=95))) == "3 months ago"
+
+
+def test_day_years():
+    assert (pretty_date(datetime.utcnow() - timedelta(days=750))) == "2 years ago"
+
+
+
+
+
+
+
+
