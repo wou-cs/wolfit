cdde90e2c7ded6f6e19e6f283341f458572e342f
Chris Brooks <chris@chrisbrooks.org>
2023-01-14 13:21:18 -0500

    Excluding sample data load command from test coverage

--- cov.sh
+++ cov.sh
@@ -3,6 +3,6 @@
 export WOLFIT_SETTINGS=$(pwd)/test.settings
 export FLASK_ENV=test
 export FLASK_DEBUG=0
-coverage run --source "." -m pytest
+coverage run --source "." --omit app/commands.py -m pytest
 coverage html
 open htmlcov/index.html
