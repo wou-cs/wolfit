53558a0701820ceff6213699e5ccfb97eb7da4ca
Chris Brooks <chris@chrisbrooks.org>
2024-01-26 17:39:31 +0100

    Decided to simplify the script as flask db upgrade is safe to run if DB exists

--- create_dev_db.sh
+++ create_dev_db.sh
@@ -3,13 +3,5 @@ export FLASK_ENV=development
 export FLASK_DEBUG=0
 export WOLFIT_SETTINGS=$(pwd)/dev.settings
 
-set -o allexport
-source $WOLFIT_SETTINGS
-set +o allexport
-
-if [ -f $BLOG_DATABASE_NAME ]; then
-   echo "Development DB ($BLOG_DATABASE_NAME) already exists -- delete first if you want to re-create."
-else
-   flask db upgrade
-fi
+flask db upgrade
 
