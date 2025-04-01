9854f396110d12db2649053d0fe184c868a9e90b
Chris Brooks <chris@chrisbrooks.org>
2025-04-01 10:47:09 -0700

    Improve coverage call to omit tests and commands

--- cov.sh
+++ cov.sh
@@ -3,7 +3,7 @@
 export WOLFIT_SETTINGS=$(pwd)/test.settings
 export FLASK_ENV=test
 export FLASK_DEBUG=0
-coverage run --source "." --omit app/commands.py -m pytest
+coverage run --source "app/" --omit "app/commands.py"  -m pytest
 coverage html
 open htmlcov/index.html
 
